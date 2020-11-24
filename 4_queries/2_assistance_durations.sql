1572435b51f6069ab028896749babb4df16942ee
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-24 21:47:54 +0000

    BootcampX Queries 4 the 'Assistance Requests Data' task Correction

--- 4_queries/2_assistance_durations.sql
+++ 4_queries/2_assistance_durations.sql
@@ -6,5 +6,5 @@ SELECT
 FROM assistance_requests
 JOIN students ON student_id = students.id
 JOIN teachers ON teacher_id = teachers.id
-JOIN assignments ON assignment_id = assignments.id
+LEFT JOIN assignments ON assignment_id = assignments.id
 ORDER BY duration;
